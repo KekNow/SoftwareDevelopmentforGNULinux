b0b2555593780b87df17090040f71162eee2794a
KekNow <49501150+KekNow@users.noreply.github.com>
2021-11-21 23:56:41 +0300

    Add files via upload

--- 08_Regexps/test.sh
+++ 08_Regexps/test.sh
@@ -9,4 +9,13 @@ else
 	exit -1
 fi
 
+gold=($(echo 'aaahaaaddggghhhaaa' | sed -E ' s/[a-d]+(.*g)/000/'))
+output=($(./esub 'aaahaaaddggghhhaaa' '[a-d]+(.*g)' '000'))
+if [[ $gold == $output ]]; then
+	echo "Test 2 passed"
+else
+	echo "Error. Test 2 not passed"
+	exit -1
+fi
+
 echo "TESTS DONE"
